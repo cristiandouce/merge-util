0bf730b87c0c250e1926f76e09c73e480e44c24d
Guido <gvilarino@gmail.com>
2015-01-23 14:55:18 -0800

    Prevent attributes with { }, undefined, null or 0 as values to be lost in the merge

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ var has = Object.prototype.hasOwnProperty;
 
 try {
   var type = require('type-component');
+  var isEmpty = require('is-empty');
 } catch (err) {
   var type = require('type');
+  var isEmpty = require('is-empty');
 }
 
 /**
@@ -31,7 +33,6 @@ function merge (a, b, opts){
     ? { inheritance: opts }
     : opts || { inheritance: false , shallow: false }
 
-
   for (var key in b) {
     var copy = !!opts.inheritance
      ? b[key] != null
@@ -40,7 +41,13 @@ function merge (a, b, opts){
     if (copy) {
       if (!a) a = {};
       if (!opts.shallow && 'object' === type(b[key])) {
-        a[key] = merge(a[key], b[key], opts);
+        if (isEmpty(a[key] && isEmpty(b[key]))) {
+          // Preserve { }, null, undefined, 0 as they were
+          a[key] = b[key];
+        } else {
+          // merge recursively
+          a[key] = merge(a[key], b[key], opts);
+        }
       } else {
         a[key] = b[key];
       }
